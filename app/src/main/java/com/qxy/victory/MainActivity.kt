9fcd146420c824b1eef645b0cd0bbaa9a700c78f
DG <goodevilbot@gmail.com>
2022-08-10 14:18:45 +0800

    :bug: Fix DouYinEntryActivity Override

--- app/src/main/java/com/qxy/victory/MainActivity.kt
+++ app/src/main/java/com/qxy/victory/MainActivity.kt
@@ -38,8 +38,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun sendAuth(): Boolean {
         val request = Authorization.Request()
-        request.scope = "user_info,trial.whitelist" // 用户授权时必选权限
-        request.optionalScope0 = "mobile" // 用户授权时可选权限（默认选择）
+        request.scope = "trial.whitelist,following.list" // 用户授权时必选权限
+        request.optionalScope0 = "fans.list" // 用户授权时可选权限（默认选择）
         request.state = "ww" // 用于保持请求和回调的状态，授权请求后原样带回给第三方。
         return douYinOpenApi.authorize(request) // 优先使用抖音app进行授权，如果抖音app因版本或者其他原因无法授权，则使用wap页授权
     }
--- app/src/main/java/com/qxy/victory/douyinapi/DouYinEntryActivity.kt
+++ app/src/main/java/com/qxy/victory/douyinapi/DouYinEntryActivity.kt
@@ -2,7 +2,6 @@ package com.qxy.victory.douyinapi
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -20,8 +19,8 @@ class DouYinEntryActivity : AppCompatActivity(), IApiEventHandler {
     lateinit var douYinOpenApi: DouYinOpenApi
 
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         douYinOpenApi = DouYinOpenApiFactory.create(this)
         douYinOpenApi.handleIntent(intent, this)
     }
